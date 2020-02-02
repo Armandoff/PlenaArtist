52641cc3f97a78f266a3ed861a68ba9e7597553b
Armando Fernandes <armando.fernandes@live.com>
2020-02-02 11:18:16 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,52 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AuthService } from 'src/app/core/services/auth.service';
+import { AuthService } from "src/app/core/services/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
-export class AppComponent   {
-
-  pages: { url: string, direction: string, icon: string, text: string }[];
+export class AppComponent {
+  pages: { url: string; direction: string; icon: string; text: string }[];
   user: firebase.User;
 
   constructor(
-              private authService: AuthService,
-              private platform: Platform,
-              private splashScreen: SplashScreen,
-              private statusBar: StatusBar
+    private authService: AuthService,
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-
     this.pages = [
-      { url: '/makeup-profile', direction: 'forward', icon: 'document', text: 'Meu Perfil'},
-      { url: '/makeup', direction: 'forward', icon: 'apps', text: 'Maquiadoras'},
-      { url: '/edit-profile', direction: 'forward', icon: 'calendar', text: 'Configurações'},
-      { url: '/makeup-profile', direction: 'forward', icon: 'business', text: 'Pendente'},
+      {
+        url: "/makeup-profile",
+        direction: "forward",
+        icon: "document",
+        text: "Meu Perfil"
+      },
+      {
+        url: "/makeup",
+        direction: "forward",
+        icon: "apps",
+        text: "Maquiadoras"
+      },
+      {
+        url: "/edit-profile",
+        direction: "forward",
+        icon: "calendar",
+        text: "Configurações"
+      }
+      // { url: '/makeup-profile', direction: 'forward', icon: 'business', text: 'Pendente'},
     ];
 
-    this.authService.authState$.subscribe( user => this.user = user );
+    this.authService.authState$.subscribe(user => (this.user = user));
 
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
